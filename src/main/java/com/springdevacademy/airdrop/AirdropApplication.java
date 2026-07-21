7b35542c4006e25c85d555db398912b656ea8f6c
G RAMESHKANNAN <98211755+grkannan@users.noreply.github.com>
2026-07-21 13:02:17 +0530

    Project Created and added to Repo

---
+++ src/main/java/com/springdevacademy/airdrop/AirdropApplication.java
@@ -0,0 +1,13 @@
+package com.springdevacademy.airdrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirdropApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirdropApplication.class, args);
+	}
+
+}
